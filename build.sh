7b75b853c6ae5bb4a7e8ff0896b4dbd258e339b4
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2022-06-16 13:55:10 +0200

    update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 VERSION="1.${GITHUB_RUN_NUMBER}.${GITHUB_RUN_ID}"
 echo "Build command line tool with version $VERSION"
 rm -f ./todo ./todo.exe
+go mod tidy
 CGO_ENABLED=0 go build -o todo -ldflags="-X 'github.com/cklukas/todo/cmd.AppVersion=$VERSION'" && \
 GOOS=windows GOARCH=amd64 go build -o todo.exe -ldflags="-X 'github.com/cklukas/todo/cmd.AppVersion=$VERSION'" && \
 chmod +x ./todo && \
