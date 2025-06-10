fb06d0bca910ab827b613149019d57569dd8b033
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2025-06-10 22:23:15 +0200

    test: fix locale tests on mac

--- internal/ui/datefmt_test.go
+++ internal/ui/datefmt_test.go
@@ -25,6 +25,7 @@ func TestIsoTimeToLocal(t *testing.T) {
 	iso := "2025-03-23T14:11:51Z"
 
 	os.Unsetenv("LC_TIME")
+	os.Unsetenv("AppleLocale")
 	t1, _ := time.Parse(time.RFC3339, iso)
 	want := t1.Local().Format(dueLayout() + " 15:04")
 	if got := isoTimeToLocal(iso); got != want {
@@ -32,10 +33,12 @@ func TestIsoTimeToLocal(t *testing.T) {
 	}
 
 	os.Setenv("LC_TIME", "en_US")
+	os.Setenv("AppleLocale", "en_US")
 	t2, _ := time.Parse(time.RFC3339, iso)
 	want = t2.Local().Format(dueLayout() + " 15:04")
 	if got := isoTimeToLocal(iso); got != want {
 		t.Fatalf("isoTimeToLocal US got %q want %q", got, want)
 	}
 	os.Unsetenv("LC_TIME")
+	os.Unsetenv("AppleLocale")
 }
